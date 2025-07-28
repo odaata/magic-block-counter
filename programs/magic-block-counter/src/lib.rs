ddfca556600f4da029c9b4f64396f5253be915bc
Mike Olson <mike@madtownmining.com>
2025-07-27 21:20:50 -0500

    Initial commit.

--- programs/magic-block-counter/src/lib.rs
+++ programs/magic-block-counter/src/lib.rs
@@ -5,17 +5,143 @@ pub mod state;
 
 use anchor_lang::prelude::*;
 
+use ephemeral_rollups_sdk::anchor::{commit, delegate, ephemeral};
+use ephemeral_rollups_sdk::cpi::DelegateConfig;
+
 pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
 declare_id!("6WwMGFVL9qnFozZAJx9aF1Y7RUC8L3hLcYycBogHNyps");
 
+pub const TEST_PDA_SEED: &[u8] = b"test-pda";
+
+#[ephemeral]
 #[program]
 pub mod magic_block_counter {
     use super::*;
+    use ephemeral_rollups_sdk::ephem::{commit_accounts, commit_and_undelegate_accounts};
+    use crate::instruction::IncrementAndCommit;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+        let counter: &mut Account<'_, Counter> = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
     }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter: &mut Account<'_, Counter> = &mut ctx.accounts.counter;
+        counter.count += 1;
+        if (counter.count > 10_000) {
+            counter.count = 0;
+        }
+        Ok(())
+    }
+
+    pub fn delegate(ctx: Context<DelegateCounter>) -> Result<()> {
+        ctx.accounts.delegate_counter(
+            &ctx.accounts.payer,
+            &[TEST_PDA_SEED],
+            DelegateConfig::default(),
+        )?;
+        Ok(())
+    }
+
+    pub fn increment_and_commit(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        let counter: &mut Account<'_, Counter> = &mut ctx.accounts.counter;
+        counter.count += 1;
+        commit_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
+
+    pub fn commit(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        commit_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn undelegate(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        commit_and_undelegate_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
+
+    pub fn increment_and_undelegate(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        let counter: &mut Account<'_, Counter> = &mut ctx.accounts.counter;
+        counter.count += 1;
+        commit_and_undelegate_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
+}
+
+#[delegate]
+#[derive(Accounts)]
+pub struct DelegateCounter<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(mut, seeds = [TEST_PDA_SEED], bump, del)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = 8 + 8,
+        seeds = [TEST_PDA_SEED],
+        bump,
+    )]
+    pub counter: Account<'info, Counter>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(
+        mut,
+        seeds = [TEST_PDA_SEED],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[commit]
+#[derive(Accounts)]
+pub struct IncrementAndCommit<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(mut, seeds = [TEST_PDA_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+    pub bump: u8,
 }
